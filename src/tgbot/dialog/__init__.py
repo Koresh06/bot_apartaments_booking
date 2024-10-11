e25f9506a3ba0fdc3ba8da75c08484238f911ae3
Koresh06 <korets-24@mail.ru>
2024-10-11 12:01:08 +0300

    add dialogs/__init__.py get_routers

--- src/tgbot/dialog/__init__.py
+++ src/tgbot/dialog/__init__.py
@@ -1,19 +1,4 @@
-all = (
-    "register_landlord_dialog",
-    "menu_loandlord_dialog",
-    "register_apartament_dialog",
-    "my_apartmernt_landlord_dialog",
-    "edit_apartment_dialog",
-    "filter_catalog_apartments_dialog",
-    "city_filter_apartment_dialog",
-    "catalog_users_apartments_dialog",
-    "price_range_filter_dialog",
-    "rooms_filter_dialog",
-    "booking_apartment",
-    "confirm_booking_landlord_dialog",
-    "register_name_city_dialog",
-    "main_admin_dialog",
-)
+from aiogram import Router
 
 from .apartments_landlord.landlord_apratments_dilalog import (
     menu_loandlord_dialog,
@@ -31,3 +16,22 @@ from .apartments_users.apartments_filters_catalog import (
 )
 from .booking_apartment.apartment_booking import booking_apartment, confirm_booking_landlord_dialog
 from .admin.admin_dialog import register_name_city_dialog, main_admin_dialog
+
+
+def get_routers() -> list[Router]:
+    return [
+        menu_loandlord_dialog,
+        register_apartament_dialog,
+        my_apartmernt_landlord_dialog,
+        edit_apartment_dialog,
+        register_landlord_dialog,
+        filter_catalog_apartments_dialog,
+        city_filter_apartment_dialog,
+        catalog_users_apartments_dialog,
+        price_range_filter_dialog,
+        rooms_filter_dialog,
+        booking_apartment,
+        register_name_city_dialog,
+        main_admin_dialog,
+        confirm_booking_landlord_dialog
+    ]
